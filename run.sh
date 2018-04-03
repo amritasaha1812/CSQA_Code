09f83b48e0a34439e2ea6b3bda93005d866f2f54
amritasaha1812 <amrita.saha87@gmail.com>
2018-04-03 07:57:14 -0400

    added run and run_test.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+#EXAMPLE RUN: python run_model.py <DUMP_DIR>
 cp $1/params.py .
-jbsub -mem 350g -queue $2 -err $1/e.txt -out $1/o.txt -require k80 python run_model.py $1 $3
+#jbsub -mem 350g -queue $2 -err $1/e.txt -out $1/o.txt -require k80 python run_model.py $1 $3
+python run_model.py $1
+
