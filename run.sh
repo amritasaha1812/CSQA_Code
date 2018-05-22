fdcf9c82d407faa2d23a6c5c38d5182dd9c1bd87
amritasaha1812 <amrita.saha87@gmail.com>
2018-05-22 06:05:45 -0400

    pushed minor changes in sh files

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #EXAMPLE RUN: python run_model.py <DUMP_DIR>
 cp $1/params.py .
-jbsub -mem 350g -queue $2 -err $1/e.txt -out $1/o.txt -require k80 python run_model.py $1
-#python run_model.py $1
+python run_model.py $1
 
